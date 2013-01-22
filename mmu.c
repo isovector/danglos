d75117937cfd166ecede8bbf23fdb772ba792eb2
Sandy Maguire <sandy.g.maguire@gmail.com>
2013-01-22 14:19:11 -0500

    got allocation working. reallocing freed data segfaults

--- mmu.c
+++ mmu.c
@@ -1,23 +1,41 @@
 #include "harness.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 
 memoryTable table;
 
 
-void set_bit(char *bitVector, size_t bit) {
-    bitVector[bit / 8] |= 1 << (bit % 8);
+void set_bit(char *bitVector, size_t bit, int on) {
+    if (on != 0)
+        bitVector[bit / 8] |= 1 << (bit % 8);
+    else
+        bitVector[bit / 8] &= ~(1 << (bit % 8));
+}
+
+char get_bit(char *bitVector, size_t bit) {
+    return bitVector[bit / 8] & (1 << (bit % 8));
+}
+
+size_t getBitFromAddress(void *address) {
+    size_t index;
+    index = (char*)address - MEMORY_START;
+    
+    return index / BLOCK_SIZE;
 }
 
 void* s_request_memory_block() {
     void *allocMem;
+    block *b;
     
     if (!table.block) {
         return NULL;
     }
     else if (table.block->next) {
         allocMem = table.block->start;
-        table.block = table.block->next;
+        b = table.block;
+        table.block = b->next;
+        b->next = NULL;
     }
     else
     {
@@ -25,10 +43,41 @@ void* s_request_memory_block() {
         table.block->start += BLOCK_SIZE;
     }
     
+    set_bit(table.bitVector, getBitFromAddress(allocMem), 1);
+    
     return allocMem;
 }
 
 int s_release_memory_block(void* memory_block) {
+    size_t index;
+    char *mem;
+    block *b;
+    
+    mem = (char*)memory_block;
+    
+    if (mem < (char*)MEMORY_START) {
+        return 1;
+    }
+    
+    index = mem - MEMORY_START;
+    
+    if (index % BLOCK_SIZE != 0) {
+        return 2;
+    }
+    
+    index = getBitFromAddress(memory_block);
+    
+    if (get_bit(table.bitVector, index) == 0) {
+        return 3;
+    }
+    
+    set_bit(table.bitVector, index, 0);
+    
+    b = (block*)MEMORY_START + index;
+    
+    b->next = table.block;
+    table.block = b;
+    
     return 0;
 }
 
@@ -51,16 +100,30 @@ void init() {
     for (i = 0; i < NUM_BLOCKS / 8; ++i)
         table.bitVector[i] = 0;
     
-    
     for (i = 0; i < requiredBlocks; ++i)
-        set_bit(table.bitVector, i);
+        set_bit(table.bitVector, i, 1);
     
     startBlock = table.block;
-    startBlock->start = (char*)&startBlock;
+    startBlock->start = (char*)startBlock;
     startBlock->end = MEMORY_START + MEMORY_SIZE;
     startBlock->next = NULL;
 }
 
 int main() {
+    void *a, *b, *c;
+    init();
     
+    a = s_request_memory_block();
+    b = s_request_memory_block();
+    c = s_request_memory_block();
+    
+    printf("%x\n", a);
+    printf("%x\n", b);
+    printf("%x\n", c);
+    printf("%d\n", s_release_memory_block(a));
+    printf("%d\n", s_release_memory_block(a));
+    printf("%d\n", s_release_memory_block((char*)b + 1));
+    printf("%d\n", s_release_memory_block(NULL));
+    
+    printf("%x\n", s_request_memory_block());
 }
