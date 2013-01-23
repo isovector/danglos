96d14b06b0ef4a908d620f453b7b13621fc709f7
Sandy Maguire <sandy.g.maguire@gmail.com>
2013-01-23 09:23:55 -0500

    Made some reasonable sizes. Added support for hardware

--- harness.h
+++ harness.h
@@ -6,13 +6,15 @@
 #define EMULATE
 
 #ifdef EMULATE
-#define MEMORY_SIZE 65536000
-#define BLOCK_SIZE 65535
-#define NUM_BLOCKS MEMORY_SIZE / BLOCK_SIZE
+#define MEMORY_SIZE 0x8000
 char MEMORY_START[MEMORY_SIZE];
 #else
+#define MEMORY_SIZE 0x8000
+#define MEMORY_START ((char*)&Image$$RW_IRAM1$$ZI$$Limit)
 #endif
 
+#define BLOCK_SIZE 128
+#define NUM_BLOCKS MEMORY_SIZE / BLOCK_SIZE
 
 typedef struct structBlock {
     char *start, *end;
