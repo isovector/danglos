7909f9715f5238f76c14401599255094c802a2dc
unknown <jlabraha@ece-rtos23.NEXUS.UWATERLOO.CA>
2013-02-27 11:26:35 -0500

    added timer code

---
+++ ContextSwitch/src/timer.c
@@ -0,0 +1,31 @@
+#include <LPC17xx.h>
+#include "timer.h"
+
+volatile int uint32_t g_timer = 0;
+
+void timer_init()
+{
+	LPC_TIM_TypeDef  * pTimer = (LPC_TIM_TypeDef *)LPC_TIM0;
+	pTimer->PR = 12499;
+	pTimer->MR0 = 1;
+	pTimer->MCR = 3;
+	g_timer = 0;
+	NVIC_EnableIRQ(TIMER0_IRQn);
+	pTimer->TCR = 1;
+}
+__asm void TIMER0_IRQHandler(void)
+{
+	PRESERVE8
+	IMPORT c_TIMER0_IRQHandler
+	PUSH{r4-r11, lr}
+	BL c_TIMER0_IRQHandler
+	POP{r4-r11, pc}
+} 
+
+void c_TIMER0_IRQHandler(void)
+{
+	/* ack inttrupt, see section  21.6.1 on pg 493 of LPC17XX_UM */
+	LPC_TIM0->IR = 1;  
+	
+	++g_timer;
+}
---
+++ ContextSwitch/src/timer.h
@@ -0,0 +1,6 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+extern void timer_init();
+
+#endif//_TIMER_H_
