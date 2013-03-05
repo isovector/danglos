980c9622b78aba4e05a3a715cbca526c1fefdd46
Sandy Maguire <sandy.g.maguire@gmail.com>
2013-03-05 12:10:47 -0500

    Added error.c

---
+++ ContextSwitch/src/error.c
@@ -0,0 +1,10 @@
+#include "error.h"
+
+const char *err_lookup(error_t err) {
+    switch(err) {
+#define error(m) case m: return #m + 4;
+#include "error_list.h"
+#undef error
+        default: return "SUCCESS";
+    }
+}
--- ContextSwitch/src/error.h
+++ ContextSwitch/src/error.h
@@ -11,13 +11,6 @@ typedef enum {
 } error_t;
 
 
-const char *err_lookup(error_t err) {
-    switch(err) {
-#define error(m) case m: return #m + 4;
-#include "error_list.h"
-#undef error
-        default: return "SUCCESS";
-    }
-}
+extern const char *err_lookup(error_t err);
 
 #endif
