0dd457dd34bf9cbf2a4d64d8fc41fb24ad123dce
Desiye Collier <collierdnc@sympatico.ca>
2013-03-07 14:34:46 -0500

    made debug print not look as stupid

--- ContextSwitch/src/debug_print.h
+++ ContextSwitch/src/debug_print.h
@@ -11,24 +11,12 @@
 void debugPrint(unsigned char * str)
 {
 	static unsigned char * GROUP_SIGNATURE = "G019_test: ";
-	/* Disable RBR, enable THRE before trying to write*/ 
-	LPC_UART0->IER = IER_THRE | IER_RLS; 
-	uart0_send_string(GROUP_SIGNATURE);		
-	/* Re-enable RBR, THRE left as enabled */
-	LPC_UART0->IER = IER_THRE | IER_RLS | IER_RBR;
-	
-	/* Disable RBR, enable THRE before trying to write*/ 
-	LPC_UART0->IER = IER_THRE | IER_RLS; 
-	uart0_send_string(str);	
-	/* Re-enable RBR, THRE left as enabled */
-	LPC_UART0->IER = IER_THRE | IER_RLS | IER_RBR;
-	
-	/* Disable RBR, enable THRE before trying to write*/ 
-	LPC_UART0->IER = IER_THRE | IER_RLS; 
-	uart0_send_string( "\r\n");
-	/* Re-enable RBR, THRE left as enabled */
-	LPC_UART0->IER = IER_THRE | IER_RLS | IER_RBR;
 
+	uart0_send_string(GROUP_SIGNATURE);		
+
+	uart0_send_string(str);	
+
+	uart0_send_string( "\r\n");
 }
 
 #else
